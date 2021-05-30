e3c697edc85aaff9bbc65e086a375519f695e024
unknown <azalshox.akbarshox.2012@gmail.com>
2021-05-30 07:44:29 +0500

    second commit

--- mysite/mentor/admin.py
+++ mysite/mentor/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from models import *
 
-# Register your models here.
+admin.site.register(About)
+admin.site.register(Trainer)
+admin.site.register(Courses)
+admin.site.register(Students)
+admin.site.register(Events)
--- mysite/mentor/models.py
+++ mysite/mentor/models.py
@@ -6,3 +6,53 @@ class About(models.Model):
     description = models.CharField(blank=False, null=False)
     image = models.ImageField(upload_to='images/', blank=False, null=False)
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Trainer(models.Model):
+    name = models.CharField(max_length=150, blank=False, null=False)
+    description = models.CharField(blank=False, null=False)
+    image = models.ImageField(upload_to='images/', blank=False, null=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Courses(models.Model):
+    name = models.CharField(max_length=150, blank=False, null=False)
+    title = models.CharField(max_length=250, blank=False, null=False)
+    description = models.CharField(blank=False, null=False)
+    image = models.ImageField(upload_to='images/', blank=False, null=False)
+    price = models.IntegerField(blank=False, null=False, default=0)
+    trainer = models.ForeignKey(Trainer, blank=False, null=False, on_delete=models.SET_NULL)
+    time = models.TimeField(auto_now=False, auto_now_add=False)
+    available_seats = models.IntegerField(blank=False, null=False, default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Events(models.Model):
+    title = models.CharField(max_length=250, blank=False, null=False)
+    description = models.CharField(blank=False, null=False)
+    image = models.ImageField(upload_to='images/', blank=False, null=False)
+    time = models.TimeField(auto_now=False, auto_now_add=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Students(models.Model):
+    full_name = models.CharField(max_length=150, blank=False, null=False)
+    email = models.EmailField(blank=False, null=False)
+    subject = models.CharField(blank=False, null=False)
+    message = models.CharField(blank=False, null=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.full_name
